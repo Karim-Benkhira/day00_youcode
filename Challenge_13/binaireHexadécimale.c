855ac22598644d59cd2b333294f278d8f7f13833
Karim Benkhira <karimbenkhira.contact@gmail.com>
2024-09-10 11:44:40 +0200

    update

--- Challenge_13/binaireHexadécimale.c
+++ Challenge_13/binaireHexadécimale.c
@@ -10,12 +10,33 @@ int LireNumber()
     return Number;
 }
 
-void afficherBinaire(int N)
+/*void afficherBinaire(int N)
 {
     if(N > 1)
         afficherBinaire(N / 2);
 
     printf("%d",N % 10);
+}*/
+
+void afficherBinaire(int N)
+{
+    int i;
+    int B[1000];
+    i = 0;
+
+    while (N > 0)
+    {
+        B[i] = N % 2;
+        N = N / 2;
+
+        i++;
+    }
+
+    for (int j= i-1; j >= 0; j--)
+    {
+        printf("%d",B[j]);
+    }
+    
 }
 
 int main()
